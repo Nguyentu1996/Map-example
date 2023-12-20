a3b4dd9b1e550170ecaf0f837900bece345d115f
Nguyentu1996 <nguyentu19962710@gmail.com>
2023-12-20 09:01:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import { GeoJSON, MapContainer, TileLayer, useMap } from "react-leaflet";
+import "./App.css";
+import jsonfile from './south-korea_02.json';
+
+function MyComponent({ position }) {
+  const map = useMap();
+  const onEachFeature = (feature, layer) => {
+    layer.on('click', (e) => {
+      console.log(e);
+      console.log(map);
+      
+      map.flyTo(e.latlng, 10);
+      
+    });
+  };
+  return <GeoJSON data={jsonfile} onEachFeature={onEachFeature} />
+}
 
 function App() {
+  const [position, setPosition] = useState([36.72067, 127.9615])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        center={position}
+        zoom={7}
+        scrollWheelZoom={false}
+      >
+        <MyComponent />
+         <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+      </MapContainer>
     </div>
   );
 }
